effc27f357744258a4b550d8d4dd628d614b8c11
Eugene <ebagdasaryan@me.com>
2019-02-17 16:57:30 -0500

    adding policy e and d steps

--- lib/micro_data_core/parser.ex
+++ lib/micro_data_core/parser.ex
@@ -57,12 +57,12 @@ defmodule MicroDataCore.Parser do
       [p] ->
         case  String.split(p, "+", parts: 2, trim: true) do
           [p, sub_tree] -> [:union, parse_policy(p, counter - 1), parse_policy(sub_tree, counter - 1)]
-          ["0"] -> :null
+          ["0"] -> 0
+          ["ANYF"] -> :anyf
           [p] ->
             cond  do
               String.ends_with?(p, "*") -> [:star, String.slice(p, 0..-2)]
-              true ->
-                [:exec, String.trim(p)]
+              true -> [:exec, String.trim(p)]
             end
         end
       _ -> raise "Syntax error. Check your program syntax."
@@ -93,31 +93,58 @@ defmodule MicroDataCore.Parser do
     {:ok}
   end
 
-  def process_request(policy, program, data) do
-    IO.inspect({policy, program}, label: "Performing step on (policy, program): \n")
-    with [hd | tl] <- program,
-         policy <- d_step(policy, hd) do
-      process_request(policy, tl, execute_command(hd, data))
-    else
-      [] ->
-        case e_step(policy) do
-          1 -> {:ok, data}
-          0 -> {:error}
-        end
-      {:error} -> throw "Some problem with matching."
+  def process_request(policy, [], data) do
+    case e_step(policy) do
+      1 -> {:ok, data}
+      0 -> {:error}
     end
   end
 
+  def process_request(policy, [command | program], data) do
+    IO.puts("\n-----------------------------\n")
+    IO.inspect({policy, program}, label: "Performing step on (policy, program): \n")
+    IO.puts("\n-----------------------------\n")
+    case d_step(policy, command) do
+      :error -> {:error}
+      policy -> process_request(policy, program, execute_command(command, data))
+#      0 -> IO.puts("Early stop. No need to proceed as policy doesn't match.")
+#           {:error}
+    end
+  end
+
+
   def d_step(policy, command) do
-    IO.inspect({policy, command}, label: "D step on (policy, program): \n")
+    IO.inspect({policy, command}, label: "D step on (policy, command): ")
     case policy do
-      [:exec, c] when command == c -> [:star, 0]
-      [:concat, p1, p2] -> [:union, [:concat, d_step(p1, command), p2], [:concat, e_step(p1), d_step(p2, command)]]
-      [:union, p1, p2] -> [:union, d_step(p1, command), d_step(p2, command)]
-      [:star, p] -> [:concat, d_step([:exec, p], command), [:star, p]]
-      [0] -> 0
-      0 -> 0
+      [:exec, c] when command == c -> 1
       [:exec, c] when command != c -> 0
+      [:exec, :anyf] -> 1
+      [:concat, p1, p2] -> simplify([:union, [:concat, d_step(p1, command), p2], [:concat, e_step(p1), d_step(p2, command)]])
+      [:union, p1, p2] -> simplify([:union, d_step(p1, command), d_step(p2, command)])
+      [:star, p] -> [:concat, d_step([:exec, p], command), [:star, p]]
+      1 -> 0
+      # because D(1,C) = D(0*,C) = D(0, C).0* = 0.0* = 0
+      0 -> 0
+      _ -> :error
+    end
+  end
+
+  def simplify(policy) do
+    case policy do
+      [:concat, 0, _] -> 0
+      [:concat, _, 0] -> 0
+      [:concat, 1, p] -> simplify(p)
+      [:concat, p, 1] -> simplify(p)
+      [:concat, 1, 1] -> 1
+      [:union, 0, p] -> simplify(p)
+      [:union, p, 0] -> simplify(p)
+      [:union, 1, _] -> 1
+      [:union, _, 1] -> 1
+      [:union, 0, 0] -> 0
+      [:concat, p1, p2] -> [:concat, simplify(p1), simplify(p2)]
+      [:union, p1, p2] -> [:union, simplify(p1), simplify(p2)]
+      [:star, p] -> [:star, simplify(p)]
+      p -> p
     end
   end
 
@@ -126,9 +153,11 @@ defmodule MicroDataCore.Parser do
     case policy do
       0 -> 0
       1 -> 1
+      # conjunction
       [:concat, p1, p2] -> e_step(p1) * e_step(p2)
+      # disjunction
       [:union, p1, p2] -> max(e_step(p1), e_step(p2))
-      [:star, p] -> 1
+      [:star, _] -> 1
       _ -> 0
     end
   end
