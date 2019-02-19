e89941c2daace3619df8cd374e8b91231421f6f5
Eugene <ebagdasaryan@me.com>
2019-02-19 14:33:56 -0500

    removed complex negation tests

--- test/micro_data_core_test.exs
+++ test/micro_data_core_test.exs
@@ -2,14 +2,12 @@ defmodule MicroDataCoreTest do
   use ExUnit.Case
   #  doctest MicroDataCore
 
-
   test "exact_match" do
     policy_text = "get_data.get_data.filter_data.remove_data.get_data.return_data"
     {:ok, program_text} = File.read("test/programs/complex.txt")
     {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
     assert result == :ok
   end
-
 
   test "anyf*_in_the_middle" do
     policy_text = "get_data.get_data.filter_data.ANYF*.return_data"
@@ -47,12 +45,6 @@ defmodule MicroDataCoreTest do
     assert result == :error
   end
 
-
-
-  ##############################
-  # NEGATION TESTS
-  ##############################
-
   test "ok_neg" do
     policy_text = "a.b.!(a).c"
     program_text = "a\nb\nd\nc"
@@ -66,90 +58,6 @@ defmodule MicroDataCoreTest do
     {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
     assert result == :error
   end
-  #  test "ok_neg_concat" do
-  #    policy_text = "ANYF*.!((g.f)).ANYF*"
-  #    program_text = "g\nf"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-  ##
-  #  test "err_neg_concat" do
-  #    policy_text = "!(g.f)"
-  #    program_text = "g\nf"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :error
-  #  end
-  #
-  #  test "ok_neg_concat_first_match" do
-  #    policy_text = "!((g.f))"
-  #    program_text = "g\nq"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-  ##
-  #  test "ok_neg_union" do
-  #    policy_text = "!((g+f)).m"
-  #    program_text = "d\nm"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-  #
-  #  test "err_neg_union" do
-  #    policy_text = "!((g+f)).m.k"
-  #    program_text = "f\nm\nk"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :error
-  #  end
-  #
-  #
-  #  test "ok_neg_star_inside" do
-  #    policy_text = "!(a*)"
-  #    program_text = "b\nb\nb"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-
-  #  test "err_neg_star_inside" do
-  #    policy_text = "!(a*)"
-  #    program_text = "a\na\na"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :error
-  #  end
-  #
-  #  test "ok_neg_star_outside" do
-  #    policy_text = "(!(a))*.m"
-  #    program_text = "d\nm"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-  #
-  #  test "err_neg_outside" do
-  #    policy_text = "(!(a))*.m"
-  #    program_text = "a\na\nm"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :error
-  #  end
-  #
-  #  test "err_force_filter_get_data" do
-  #    policy_text = "((!g)*.g.f.(!g)*)*"
-  #    program_text = "s\ns\ng\nf\ns\ns\ns\ng"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :error
-  #  end
-  #
-  #
-  #  test "ok_zero" do
-  #    policy_text = "!(0)"
-  #    program_text = "a"
-  #    {result, _} = MicroDataCore.Core.entry_point([policy_text, program_text])
-  #    assert result == :ok
-  #  end
-
-
-
-  ##############################
-  # NEGATION TESTS
-  ##############################
 
   test "ok_intersect" do
     policy_text = "(a&ANYF)"
