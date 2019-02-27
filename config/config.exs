314e61d17ec1c35c813ad5c0a578eb8a9976c49c
Eugene <ebagdasaryan@me.com>
2019-02-27 08:55:41 -0500

    added new views

--- config/config.exs
+++ config/config.exs
@@ -28,7 +28,8 @@ config :phoenix, :json_library, Jason
 
 config :ancile, :pow,
   user: Ancile.Users.User,
-  repo: Ancile.Repo
+  repo: Ancile.Repo,
+  web_module: AncileWeb
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
