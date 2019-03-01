110e470885a656fefe68e6f2aad85ee2f3197923
Eugene <ebagdasaryan@me.com>
2019-03-01 08:18:02 -0500

    playing with phoenix.

--- lib/ancile_web/controllers/app_controller.ex
+++ lib/ancile_web/controllers/app_controller.ex
@@ -2,7 +2,10 @@ defmodule AncileWeb.AppController do
   use AncileWeb, :controller
 
   def app_dashboard(conn, _params) do
-    render(conn, "app_dashboard.html")
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("app_dashboard.html")
   end
 
   def serve_add_page(conn, _params), do: render(conn, "add_program.html")
