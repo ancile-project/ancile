0601e5a2eeadc3376433916d03c9c89440b7c786
Eugene <ebagdasaryan@me.com>
2019-03-18 22:57:20 -0400

    updated Python implementation

--- src/micro_data_core_python/core.py
+++ src/micro_data_core_python/core.py
@@ -1,30 +1,30 @@
 from src.micro_data_core_python.policy_sly import PolicyParser
-import src.micro_data_core_python.policy_processor as decorator
 from src.micro_data_core_python.functions import *
 
 
+def execute(policies, program, sensitive_data=None):
+    parsed_policies = dict()
+    for provider, policy in policies.items():
+        parsed_policies[provider] = PolicyParser.parse_it(policy.decode('ascii'))
 
-
-def execute(policy, program, sensitive_data=None):
-
-    parsed_policy = PolicyParser.parse_it(policy.decode('ascii'))
-    decorator.PolicyProcessor.current_policy = parsed_policy
-    print(f'\nparsed policy: {decorator.PolicyProcessor.current_policy}')
+    UserSpecific._user_policies = parsed_policies
+    print(f'\nparsed policies: {UserSpecific._user_policies}')
     program = program.decode('ascii')
     print(f'submitted program:\n{program}\n')
-    data = []
-    try:
-        exec(program)
-    except ValueError:
-        print('Policy did not pass.')
-        return False
-    return data
+    result = []
+    exec(program)
+
+    return result
 
 
 if __name__ == '__main__':
-
-    res = execute(b'asdf.qwer', b'data.append(5)\nasdf()\nqwer()', None)
+    res = execute({'dataA': b'fetch_test_data.asdf.qwer.return_data'},
+b'''
+data_pair=fetch_test_data(data_source="dataA")
+asdf(data=data_pair)
+qwer(data=data_pair)
+result.append(return_data(data=data_pair))
+''',
+                  None)
     print(f'POLICY EVALUATED TO {res}\n')
-    res = execute(b'qwer.asdf', b'data.append(5)\nqwer()\nqwer()', None)
-    print(f'POLICY EVALUATED TO {res}')
 
---
+++ src/micro_data_core_python/user_specific.py
@@ -0,0 +1,62 @@
+from src.micro_data_core_python.datapolicypair import DataPolicyPair
+
+
+class UserSpecific:
+
+    _user_policies = dict()
+    _user_tokens = dict()
+
+    @classmethod
+    def transform_decorator(cls, f):
+        def wrapper(*args, **kwargs):
+            print(f'args: {args}')
+            print(f'kwargs: {kwargs}')
+            if args:
+                # let's just ask to specify kwargs. Useful for policy creation.
+                raise ValueError("Please specify keyword arguments instead of positions.")
+
+            dp_pair = kwargs.get('data', False)
+            if isinstance(dp_pair, DataPolicyPair):
+                return dp_pair.call(f, *args, **kwargs)
+            else:
+                raise ValueError("You need to provide a Data object. Use get_data to get it.")
+
+        return wrapper
+
+    @classmethod
+    def get_data_decorator(cls, f):
+        def wrapper(*args, **kwargs):
+            print(f'args: {args}')
+            print(f'kwargs: {kwargs}')
+            if args:
+                # let's just ask to specify kwargs. Useful for policy creation.
+                raise ValueError("Please specify keyword arguments instead of positions.")
+            ds = kwargs.get('data_source', False)
+            if ds:
+                policy = cls._user_policies[ds]
+                dp_pair = DataPolicyPair(policy)
+                kwargs['data'] = dp_pair
+                kwargs['token'] = cls._user_tokens.get('token', None)
+                dp_pair.call(f, *args, **kwargs)
+                return dp_pair
+            else:
+                raise ValueError("Please specify parameter: data_source.")
+
+        return wrapper
+
+    @classmethod
+    def return_data_decorator(cls, f):
+        def wrapper(*args, **kwargs):
+            print(f'args: {args}')
+            print(f'kwargs: {kwargs}')
+            if args:
+                # let's just ask to specify kwargs. Useful for policy creation.
+                raise ValueError("Please specify keyword arguments instead of positions.")
+
+            dp_pair = kwargs.get('data', False)
+            if isinstance(dp_pair, DataPolicyPair):
+                return dp_pair.return_data(f, *args, **kwargs)
+            else:
+                raise ValueError("You need to provide a Data object. Use get_data to get it.")
+
+        return wrapper
