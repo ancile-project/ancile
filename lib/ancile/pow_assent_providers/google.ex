95655e972ae223baa69d74abd1767dc4be9942be
Griffin Berlstein <griffred20@gmail.com>
2019-03-21 15:03:33 -0400

    sketch of google-calendar

---
+++ lib/ancile/pow_assent_providers/google.ex
@@ -0,0 +1,44 @@
+defmodule Ancile.PowAssentProviders.Google do  @moduledoc """
+  Google OAuth 2.0 strategy.
+
+  ## Usage
+
+      config :my_app, :pow_assent,
+        providers: [
+          google: [
+            client_id: "REPLACE_WITH_CLIENT_ID",
+            client_secret: "REPLACE_WITH_CLIENT_SECRET",
+            strategy: PowAssent.Strategy.Google
+          ]
+        ]
+  """
+  use PowAssent.Strategy.OAuth2.Base
+
+  @spec default_config(Keyword.t()) :: Keyword.t()
+  def default_config(_config) do
+    [
+      site: "https://www.googleapis.com",
+      authorize_url: "https://accounts.google.com/o/oauth2/v2/auth",
+      token_url: "https://www.googleapis.com/oauth2/v4/token",
+      authorization_params: [scope: "https://www.googleapis.com/auth/calendar.readonly" ]
+    ]
+  end
+
+  @spec normalize(Keyword.t(), map()) :: {:ok, map()}
+  def normalize(_config, user) do
+    {:ok, %{
+      "uid"        => user["id"],
+      "name"       => user["name"],
+      "email"      => verified_email(user)}}
+  end
+
+  # def get_user(config, token) do
+  #   IO.inspect(config, label: "get_user config: ")
+  #   IO.inspect(token, label: "get_user token: ")
+  #   user = %{
+  #     "uid"        =>  token["refresh_token"], # this is stupid but CDS doesn't return user id
+  #     "name" => "CDS, No data available. TODO: change it."}
+
+  #     {:ok, user}
+  # end
+end
---
+++ src/micro_data_core_python/functions/google.py
@@ -0,0 +1,57 @@
+from src.micro_data_core_python.decorators import transform_decorator, external_request_decorator
+from src.micro_data_core_python.errors import AncileException
+
+@external_request_decorator
+def get_primary_calendar_metadata(data, token=None):
+    import requests
+    target_url = "https://www.googleapis.com/calendar/v3/users/me/calendarList"
+    r = requests.get(target_url, 
+            headers={'Authorization': "Bearer " + token})
+
+    if r.status_code != 200:
+        raise AncileException("Request Error")
+
+    calendar_list = r.json()
+    primary = [x for x in calendar_list['items'] if x.get("primary")]
+
+    if not primary:
+        raise AncileException("No Calendar")
+
+    data['calendar'] = primary[0]
+    return True
+
+@external_request_decorator
+def get_calendar_events_in_relative_window(data, token=None, 
+                    min_time=0, max_time=1):
+    from datetime import datetime, timedelta
+    import requests
+
+    def format_time(time):
+        return datetime.strftime("%Y-%m-%dT%H:%M:%S")
+
+    now = datetime.now()
+    lower = format_time(now - timedelta(minutes=min_time))
+    upper = format_time(now + timedelta(minutes=max_time))
+
+    calendar = data.pop('calendar')
+
+    target_url = "https://www.googleapis.com/calendar/v3/users/me/calendarList" + \
+                calendar["id"] + "/events?singleEvents=true&timeMin=" + \
+                lower + "&timeMax=" + upper
+
+    r = requests.get(target_url, 
+            headers={'Authorization': "Bearer " + token})
+    
+    if r.status_code != 200:
+        raise AncileException("Request Error")
+
+    data['events'] = r.json()['items']
+    return True
+
+@transform_decorator
+def event_occuring(data, event_title=None):
+    events = data.pop('events')
+    result = event_title in [event['summary'] for event in events]
+    data['event_occuring'] = result
+    return True
+
