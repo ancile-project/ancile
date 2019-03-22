bc979e961f76aaffb535e298a041ea74fbef8173
Griffin Berlstein <griffred20@gmail.com>
2019-03-22 14:09:51 -0400

    more oauth fighting

--- lib/ancile/pow_assent_providers/google.ex
+++ lib/ancile/pow_assent_providers/google.ex
@@ -20,7 +20,8 @@ defmodule Ancile.PowAssentProviders.Google do  @moduledoc """
       site: "https://www.googleapis.com",
       authorize_url: "https://accounts.google.com/o/oauth2/v2/auth",
       token_url: "https://www.googleapis.com/oauth2/v4/token",
-      authorization_params: [scope: "https://www.googleapis.com/auth/calendar.readonly" ],
+      authorization_params: [scope: "https://www.googleapis.com/auth/calendar.readonly",
+                              access_type: "offline" ],
       param_encoding: True
     ]
   end
@@ -32,13 +33,13 @@ defmodule Ancile.PowAssentProviders.Google do  @moduledoc """
       "name"       => user["name"]}}
   end
 
-  # def get_user(config, token) do
-  #   IO.inspect(config, label: "get_user config: ")
-  #   IO.inspect(token, label: "get_user token: ")
-  #   user = %{
-  #     "uid"        =>  token["refresh_token"], # this is stupid but CDS doesn't return user id
-  #     "name" => "CDS, No data available. TODO: change it."}
+  def get_user(config, token) do
+    IO.inspect(config, label: "get_user config: ")
+    IO.inspect(token, label: "get_user token: ")
+    user = %{
+      "uid"        =>  token["refresh_token"], # FIX LATER
+      "name" => "Google. TODO: change it."}
 
-  #     {:ok, user}
-  # end
+      {:ok, user}
+  end
 end
