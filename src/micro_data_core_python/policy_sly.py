aa21c056fd4eff06f3b9f1d794591126b8e38b98
Griffin Berlstein <griffred20@gmail.com>
2019-06-11 17:15:15 -0400

    added parameter sets

--- src/micro_data_core_python/policy_sly.py
+++ src/micro_data_core_python/policy_sly.py
@@ -103,16 +103,50 @@ class RangeCell(object):
     def __ne__(self, other):
         return not self.__eq__(other)
 
+class SetCell(object):
+    def __init__(self, name, in_objects, invert):
+        self._set = set(in_objects)
+        self._name = name
+        self._invert_flag = invert
+
+    def evaluate(self, name_val) -> bool:
+        """Evaluate the given value against the cell.
+
+        returns name_val op value
+        """
+        if not self._invert_flag:
+            return name_val in self._set
+        else:
+            return name_val not in self._set
+
+    def __repr__(self):
+        inv_str = ' ' if not self._invert_flag else ' not '
+        return f'<? SetCell: {self._name}{inv_str}in {self._set} ?>'
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+        elif not isinstance(other, SetCell):
+            return False
+        else:
+            return self._name == other._name and                              \
+                   self._invert_flag == other._invert_flag and                \
+                   self._set == other._set
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 class PolicyLexer(Lexer):
     tokens = { TEXT, NUMBER, FLOAT, UNION, CONCAT, INTERSECT,  NEG, STAR,
                ANYF, LPAREN, RPAREN, LBRACKET, RBRACKET, COMMA, EQ,
-               PRIVATE, STRING, NEQ, LEQ, LE, GEQ, GE }
+               PRIVATE, STRING, NEQ, LEQ, LE, GEQ, GE, LBRACE, RBRACE, IN }
     ignore = ' \s\t\n\r'
 
 
     ANYF = 'ANYF'
     PRIVATE = 'private'
+    IN = 'in'
     # Tokens
     TEXT = r'[a-zA-Z_][a-zA-Z0-9_]*'
     STRING = r'\"[a-zA-Z_][a-zA-Z0-9_:/\.]*\"'
@@ -129,8 +163,8 @@ class PolicyLexer(Lexer):
     RPAREN = r'\)'
     LBRACKET = r'\['
     RBRACKET = r'\]'
-    # LBRACE = r'\{'
-    # RBRACE = r'\}'
+    LBRACE = r'\{'
+    RBRACE = r'\}'
     # QUOTE = r'\"'
     COMMA = r'\,'
     NEQ = r'\!\='
@@ -280,6 +314,32 @@ class PolicyParser(Parser):
             raise ParseError("Invalid range")
 
         return {p.TEXT: RangeCell(p.TEXT, lower, upper, type_val, True)}
+
+    @_('TEXT IN LBRACE sets RBRACE')
+    def param(self, p):
+        return {p.TEXT: SetCell(p.TEXT, p.sets, False)}
+
+    @_('NEG TEXT IN LBRACE sets RBRACE')
+    def param(self, p):
+        return {p.TEXT: SetCell(p.TEXT, p.sets, True)}
+
+    @_('set_elem COMMA sets')
+    def sets(self, p):
+        element = [p.set_elem]
+        elements = element + p.sets
+        return elements
+
+    @_('set_elem')
+    def sets(self, p):
+        return [p.set_elem]
+
+    @_('numeric')
+    def set_elem(self, p):
+        return p.numeric
+
+    @_('STRING')
+    def set_elem(self, p):
+        return p.STRING.strip('"')
 
     @_('NUMBER')
     def numeric(self, p):
