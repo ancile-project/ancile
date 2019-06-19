6f34fc01cfe2849d4979fae5a144ee80030dbba1
Griffin Berlstein <griffred20@gmail.com>
2019-06-19 17:15:23 -0400

    automatically generate admin, user, app

--- scripts/setup/init_db.py
+++ scripts/setup/init_db.py
@@ -1,8 +1,16 @@
 from ancile_web import app, db, user_datastore
 
 with app.app_context():
-  user_datastore.find_or_create_role("admin")
-  user_datastore.find_or_create_role("app")
-  user_datastore.find_or_create_role("user")
+    user_datastore.find_or_create_role("admin")
+    user_datastore.find_or_create_role("app")
+    user_datastore.find_or_create_role("user")
 
-  db.session.commit()
+    admin = user_datastore.create_user(email="admin", password="password")
+    user = user_datastore.create_user(email="user", password="user_password")
+    app = user_datastore.create_user(email="app", password="app_password")
+
+    user_datastore.add_role_to_user(admin, "admin")
+    user_datastore.add_role_to_user(user, "user")
+    user_datastore.add_role_to_user(app, "app")
+
+    db.session.commit()
