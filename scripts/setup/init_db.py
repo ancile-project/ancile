2f1fb3c1c1a5cc5f03853db2be8cd923a381b2a0
Griffin Berlstein <griffred20@gmail.com>
2019-06-19 17:37:26 -0400

    Make the sample users confirmed

--- scripts/setup/init_db.py
+++ scripts/setup/init_db.py
@@ -1,6 +1,7 @@
 from ancile_web import app, db, user_datastore
 
 with app.app_context():
+    from datetime import datetime
     user_datastore.find_or_create_role("admin")
     user_datastore.find_or_create_role("app")
     user_datastore.find_or_create_role("user")
@@ -8,6 +9,10 @@ with app.app_context():
     admin = user_datastore.create_user(email="admin", password="password")
     user = user_datastore.create_user(email="user", password="user_password")
     app = user_datastore.create_user(email="app", password="app_password")
+
+    admin.confirmed_at = datetime.now()
+    user.confirmed_at = datetime.now()
+    app.confirmed_at = datetime.now()
 
     user_datastore.add_role_to_user(admin, "admin")
     user_datastore.add_role_to_user(user, "user")
--- scripts/setup/run_migrations.sh
+++ scripts/setup/run_migrations.sh
@@ -4,4 +4,6 @@ source .env/bin/activate
 export FLASK_APP=runner.py
 flask db upgrade
 
+echo "Initializing server roles ... "
+echo "Generating sample users ... "
 python -m  scripts.setup.init_db
