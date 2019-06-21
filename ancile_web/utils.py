ef71f2383ee18ebb5abce49871ebc5fc3ac1ef02
Griffin Berlstein <griffred20@gmail.com>
2019-06-21 16:03:04 -0400

    added reload signal

---
+++ ancile_web/utils.py
@@ -0,0 +1,13 @@
+from os import kill
+import signal
+from ancile_web.errors import AncileException
+
+def _get_pid():
+    with open('.pidfile') as f:
+        return int(f.readline())
+
+def reload_server():
+    try:
+        kill(_get_pid(), signal.SIGHUP)
+    except (FileNotFoundError, ValueError):
+        AncileException('Could not reload server')
