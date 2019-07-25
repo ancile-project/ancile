cda39991ea7d19c0f107ef911aab5204b0bc454a
Griffin Berlstein <griffred20@gmail.com>
2019-07-25 15:28:07 -0400

    django admin details

--- ancile/web/dashboard/admin.py
+++ ancile/web/dashboard/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from dashboard.models import *
 
 # Register your models here.
+admin.site.register(
+    [
+        User,
+        App,
+        DataProvider,
+        Policy,
+        Scope,
+        PermissionGroup,
+        PredefinedPolicy,
+        Function,
+        Token,
+        PrivateData,
+    ]
+)
