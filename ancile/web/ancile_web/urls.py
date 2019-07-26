5123790e85fe9dfd673e4d1cd720e0b7e8c0d149
Griffin Berlstein <griffred20@gmail.com>
2019-07-26 14:36:06 -0400

    remove unnecessary imports

--- ancile/web/ancile_web/urls.py
+++ ancile/web/ancile_web/urls.py
@@ -17,11 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name="login.html"), name='login'),
     path('admin/', admin.site.urls),
     path('oauth/', include('ancile.web.oauth.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
