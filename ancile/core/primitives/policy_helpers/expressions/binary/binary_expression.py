12eb02869245f8bd4e6c745639a3983d52bd79a9
Eugene <ebagdasaryan@me.com>
2019-07-29 11:50:25 -0400

    added list rules

--- ancile/core/primitives/policy_helpers/expressions/binary/binary_expression.py
+++ ancile/core/primitives/policy_helpers/expressions/binary/binary_expression.py
@@ -20,3 +20,27 @@ class BinaryExpression(BaseExpression, ABC):
             return f'({self.l_expr}){self.operation}{self.r_expr}'
         else:
             return f'({self.l_expr}){self.operation}({self.r_expr})'
+
+    @classmethod
+    def assemble_from_list(cls, expressions,
+                           empty_expr=ConstantExpression(Constants.ZERO)):
+        """Combines a passed list of expressions as a tree of
+        individual expression
+
+        """
+
+        expr_box = iter(expressions)
+
+        l_expr = next(expr_box, None)
+        if l_expr is None:
+            return empty_expr
+
+        r_expr = next(expr_box, None)
+        if r_expr is None:
+            return empty_expr
+
+        assembled_expr = cls(l_expr, r_expr)
+        for expr in expr_box:
+            assembled_expr = cls(assembled_expr, expr)
+
+        return assembled_expr
