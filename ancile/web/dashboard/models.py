96859f92aef7c4f0fb909c1f82297c15897d7581
Griffin Berlstein <griffred20@gmail.com>
2019-07-29 10:42:13 -0400

    added constraints to models

--- ancile/web/dashboard/models.py
+++ ancile/web/dashboard/models.py
@@ -4,17 +4,23 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres import fields
 from requests_oauthlib import OAuth2Session
 from base64 import b64encode
+from bcrypt import gensalt
+
 
 class User(AbstractUser):
-    pass
+    is_developer = models.BooleanField(default=False)
 
 
 class App(models.Model):
     name = models.CharField(max_length=128, unique=True)
     developers = models.ManyToManyField(User)
     description = models.TextField()
-    token_salt = models.CharField(max_length=64)
-    salt_timestamp = models.TimeField()
+    token_salt = models.CharField(max_length=64, unique=True, default=gensalt)
+    salt_timestamp = models.TimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Application"
+        verbose_name_plural = "Applications"
 
 
 class DataProvider(models.Model):
@@ -23,17 +29,17 @@ class DataProvider(models.Model):
     client_secret = models.TextField()
     access_token_url = models.TextField()
     auth_url = models.TextField()
-    extra_params = fields.JSONField()
+    extra_params = fields.JSONField(default=dict)
 
     @property
     def basic_auth_header(self):
-        return str(b64encode(bytes(self.client_id + ":" + self.client_secret, 'utf8')), 'utf-8')
+        return str(
+            b64encode(bytes(self.client_id + ":" + self.client_secret, "utf8")), "utf-8"
+        )
 
     def generate_url(self, scopes, base):
         session = OAuth2Session(
-            client_id=self.client_id,
-            redirect_uri=self.redirect_url(base),
-            scope=scopes
+            client_id=self.client_id, redirect_uri=self.redirect_url(base), scope=scopes
         )
 
         auth_url, state = session.authorization_url(self.auth_url)
@@ -45,66 +51,84 @@ class DataProvider(models.Model):
 
 class Policy(models.Model):
     text = models.TextField()
-    provider = models.ForeignKey(DataProvider,
-                                    null=True,
-                                    on_delete=models.SET_NULL)
-    user = models.ForeignKey(User,
-                                on_delete=models.CASCADE)
-    app = models.ForeignKey(App,
-                               on_delete=models.CASCADE)
+    provider = models.ForeignKey(DataProvider, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
 
-    active = models.BooleanField()
-    read_only = models.BooleanField()
+    active = models.BooleanField(default=False)
+    read_only = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Policy"
+        verbose_name_plural = "Policies"
 
 
 class Scope(models.Model):
     name = models.TextField()
-    provider = models.ForeignKey(DataProvider,
-                                    on_delete=models.CASCADE)
+    provider = models.ForeignKey(DataProvider, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "provider"], name="scope:unique_scope_provider"
+            )
+        ]
 
 
 class PermissionGroup(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
-    app = models.ForeignKey(App,
-                        on_delete=models.CASCADE)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
     scopes = models.ManyToManyField(Scope)
 
 
 class PredefinedPolicy(models.Model):
-    provider = models.ForeignKey(DataProvider,
-                                    null=True,
-                                    on_delete=models.SET_NULL)
-    app = models.ForeignKey(App,
-                               on_delete=models.CASCADE)
+    provider = models.ForeignKey(DataProvider, null=True, on_delete=models.SET_NULL)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
     approved = models.BooleanField()
-    group = models.ForeignKey(PermissionGroup,
-                                 on_delete=models.CASCADE)
+    group = models.ForeignKey(PermissionGroup, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Predefined policy"
+        verbose_name_plural = "Predefined policies"
+
 
 class Function(models.Model):
     name = models.CharField(max_length=128)
     body = models.TextField()
-    app = models.ForeignKey(App,
-                               on_delete=models.CASCADE)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
     description = models.TextField()
-    approved = models.BooleanField()
+    approved = models.BooleanField(default=False)
+
 
 class Token(models.Model):
-    user = models.ForeignKey(User,
-                                on_delete=models.CASCADE)
-    provider = models.ForeignKey(DataProvider,
-                                    on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider = models.ForeignKey(DataProvider, on_delete=models.CASCADE)
     token_type = models.TextField()
     access_token = models.TextField()
     refresh_token = models.TextField(null=True)
     expires_at = models.IntegerField()
     scopes = models.ManyToManyField(Scope)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "provider"], name="token:unique_user_provider"
+            )
+        ]
+
 
 class PrivateData(models.Model):
-    user = models.ForeignKey(User,
-                                on_delete=models.CASCADE)
-    provider = models.ForeignKey(DataProvider,
-                                    on_delete=models.CASCADE)
-    value = fields.JSONField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    provider = models.ForeignKey(DataProvider, on_delete=models.CASCADE)
+    value = fields.JSONField(default=dict)
+
+    class Meta:
+        verbose_name = "Private data"
+        verbose_name_plural = "Private data"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "provider"], name="private_data:unique_user_provider"
+            )
+        ]
