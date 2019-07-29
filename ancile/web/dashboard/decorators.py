a8ba4517b3a14e31993693fa0feca960d57ed323
Griffin Berlstein <griffred20@gmail.com>
2019-07-29 15:27:42 -0400

    bugfix for decorators

--- ancile/web/dashboard/decorators.py
+++ ancile/web/dashboard/decorators.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
-def user_is_developer(f, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+def user_is_developer(f=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
     decorator = user_passes_test(lambda u: u.is_developer,
                                  login_url=login_url,
                                  redirect_field_name=redirect_field_name)
-    return decorator(f)
+    if f:
+        return decorator(f)
+    else:
+        return decorator
 
-def user_is_admin(f, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+def user_is_admin(f=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
     decorator = user_passes_test(lambda u: u.is_superuser,
                                  login_url=login_url,
                                  redirect_field_name=redirect_field_name)
-    return decorator(f)
+    if f:
+        return decorator(f)
+    else:
+        return decorator
