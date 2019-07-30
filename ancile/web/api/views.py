32de94a33545eb47c89fd236470aeaa657b6f9da
Mohamad Safadieh <me@msafadieh.com>
2019-07-30 13:38:42 -0400

    fix permission api

--- ancile/web/api/views.py
+++ ancile/web/api/views.py
@@ -1,7 +1,7 @@
 import json
 from django.shortcuts import render
 # from ancile.core.core import execute
-from django.http import HttpRequest
+from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from ancile.web.dashboard.models import User, Token, PermissionGroup, DataProvider, App
@@ -21,9 +21,9 @@ def check_permission_group(request):
 
     provider_scope = {tk.provider:tk.scopes.all() for tk in request.user.tokens}
 
-    return [{"path_name": provider.path_name,
+    return HttpResponse(json.dumps([{"path_name": provider.path_name,
              "display_name": provider.display_name,
              "status": len(set(wanted_scopes) - set(provider_scope.get(provider, set()))) == 0,
              "scopes": [{"simple_name": sc.simple_name,
                          "value": sc.value} for sc in wanted_scopes]
-             } for provider, wanted_scopes in perm_group.provider_scope_list]
+             } for provider, wanted_scopes in perm_group.provider_scope_list]))
