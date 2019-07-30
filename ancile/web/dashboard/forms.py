0703e32b5136124266a9ef71843643a1e82a0e3b
Griffin Berlstein <griffred20@gmail.com>
2019-07-30 12:40:09 -0400

    made forms load their choices dynamically on form creation rather than definition

--- ancile/web/dashboard/forms.py
+++ ancile/web/dashboard/forms.py
@@ -4,18 +4,22 @@ from ancile.web.dashboard.models import *
 class AdminAddPolicyForm(forms.Form):
     text = forms.CharField(label="", widget=forms.Textarea)
     provider = forms.ChoiceField(label="Provider")
-    app_list = App.objects.all()
-    app_choices = [(app.name, app.name) for app in app_list]
-    app = forms.ChoiceField(label="App", choices=app_choices)
     active = forms.BooleanField(label="Active?", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(AdminAddPolicyForm, self).__init__(*args, **kwargs)
+        app_choices = [(app.name, app.name) for app in App.objects.all()]
+        self.fields['app'] = forms.ChoiceField(label="App", choices=app_choices)
 
 class AdminEditPolicyForm(forms.Form):
     text = forms.CharField(label="", widget=forms.Textarea)
     active = forms.BooleanField(label="Active?", required=False)
 
 class AdminEditUserForm(forms.Form):
-    app_list = App.objects.all()
-    choices = [(app.name, app.name) for app in app_list]
-    apps = forms.MultipleChoiceField(label="Developer For", required=False, choices=choices)
     is_admin = forms.BooleanField(label="Admin?", required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(AdminEditUserForm, self).__init__(*args, **kwargs)
+        choices = [(app.name, app.name) for app in App.objects.all()]
+        self.fields['apps'] = forms.MultipleChoiceField(label="Developer For", required=False, choices=choices)
 
