c0b2594147b37497ca1ab8c31475d16e5dfce8cd
Corin Rose <crose@vassar.edu>
2019-07-31 11:16:15 -0400

    added djnago-crispy-forms for bootstrap styling of forms

--- ancile/web/ancile_web/settings.py
+++ ancile/web/ancile_web/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
