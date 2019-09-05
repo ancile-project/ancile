5770838186d8972ba0835139e14284dd825a5f14
Mohamad Safadieh <me@msafadieh.com>
2019-09-04 23:45:25 -0400

    implement signup

--- ancile/web/dashboard/urls.py
+++ ancile/web/dashboard/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', views.dashboard),
     path('login/', views.login_view),
-    path('logout/', views.logout_view)
+    path('logout/', views.logout_view),
+    path('signup/', views.signup_view)
 ]
