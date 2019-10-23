08ec44f94d0b830216024ad66043eb60322801c6
Mohamad Safadieh <me@msafadieh.com>
2019-10-23 10:22:23 -0400

    add pending developers

--- ancile/web/frontend/src/pages/admin/AdminConsole.vue
+++ ancile/web/frontend/src/pages/admin/AdminConsole.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>
-  </div>
+  <Table 
+  header="Pending Developers"
+  :fields="fields"
+  :actions="actions"
+  :data="data"/>
 </template>
 
 <script>
+import Table from '@/components/Table';
+
 export default {
-  name: "DevConsole"
+  name: "DevConsole",
+  components: {
+    Table
+  },
+  data() {
+    return {
+      fields: [
+        {
+          title: "Name",
+          value: "fullName"
+        },
+        {
+          title: "Username",
+          value: "username"
+        }
+      ],
+      actions: [
+        {
+          color: "success",
+          icon: "fa-check",
+          callback: u => {
+            this.mutateDev(u.id, true);
+          }
+        },
+        {
+          color: "danger",
+          icon: "fa-times",
+          callback: u => {
+            this.mutateDev(u.id, false);
+          }
+        }
+      ],
+      data: [],
+    }
+  },
+  methods: {
+    async getData() {
+      const query = `{
+        pendingDevelopers {
+          id
+          username
+          firstName
+          lastName
+        }
+      }`;
+
+      await this.$root.getData(query)
+        .then(resp => {
+          this.data = resp.pendingDevelopers.map(u => ({...u, fullName: u.firstName + " " + u.lastName}));
+        });
+    },
+    async mutateDev(id, approve) {
+      const query = `
+      mutation mutateDev($id: Int, $approve: Boolean) {
+        mutateDev(user: $id, approve: $approve) {
+          ok        }
+      }
+      `
+
+      const args = {
+        id,
+        approve
+      }
+
+      const msg = approve ? "approved" : "declined";
+
+      this.$root.getData(query, args)
+        .then(resp => {
+          if (resp.mutateDev.ok) {
+            this.$root.notify("success", "Developer application " + msg);
+          }
+        })
+        .catch(() => {
+            this.$root.notify("fail", "An error has occurred.");
+          }
+        );
+    }
+  }
 }
 </script>
 
