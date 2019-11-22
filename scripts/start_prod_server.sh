d786cbc2740f56c8dae0fe17b340b4aa4399672b
Mohamad Safadieh <me@msafadieh.com>
2019-11-22 08:27:17 -0500

    fix db script path in production script

--- scripts/start_prod_server.sh
+++ scripts/start_prod_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-bash scripts/setup/start_db.sh
+bash scripts/start_db.sh
 bash scripts/setup/build_vue_prod.sh
 source .env/bin/activate;
 gunicorn  --bind 0.0.0.0:8000 ancile.web.ancile_web.wsgi:application -w 4 --pid .pidfile
