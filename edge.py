b0aff029c9ec6ef32814b301d9703f85f05a5cdf
Mohamad Safadieh <me@msafadieh.com>
2020-01-31 02:29:06 -0500

    add edge node delay support and execution time measurement

--- edge.py
+++ edge.py
@@ -2,7 +2,7 @@ import pika
 import dill
 import ancile
 from ancile.core.core import execute
-from time import sleep
+from time import sleep, time
 from socket import gethostname
 
 creds = pika.PlainCredentials('test', 'test')
@@ -10,25 +10,34 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='143.229.6.2
 channel = connection.channel()
 hostname = gethostname()
 channel.queue_declare(queue=hostname)
-print("listening on", hostname)
+
+def debug(msg):
+    print(f"[{hostname}] {msg}")
 
 def callback(ch, method, properties, body):
-    print(" [x] Received message of length {}".format(len(body)))
+    debug(f"Received message of length {len(body)}")
     request = dill.loads(body)
 
     dpp = request.get("data_policy_pair")
     program = request.get("program")
+    delay = request.get("delay")
+
+    sleep(delay)
 
     if not dpp:
         message = {"error": "data_policy_pair missing"}
     elif not program:
         message = {"error": "program missing"}
     else:
+        initial = time()
         res = execute(users_secrets=[],
                         program=program,
                         app_id=None,
                         app_module=None,
                         data_policy_pairs=[dpp])
+        delta = time() - initial
+        with open(hostname+'-delta.log', 'a') as f:
+            f.write(str(delta)+'\n')
         if res["result"] == "error":
             message = {"error": res["traceback"]}
         elif not res["data"]:
@@ -37,8 +46,8 @@ def callback(ch, method, properties, body):
             message = {"data_policy_pair": res["data"][0]}
 
     pickled_body = dill.dumps(message)
-    print("length of response:", str(len(pickled_body)))
-    print("returning response:", str(message))
+    debug(f"Length of response: {len(pickled_body)}")
+    debug(f"Returning response: {message}")
 
     index = 0
     part_num = 0
@@ -52,7 +61,7 @@ def callback(ch, method, properties, body):
                     correlation_id=properties.correlation_id,
                 ),
                 body=pickled_body[index:new_index])
-        print("sent part", str(part_num))
+        debug(f"Sent part {part_num}")
         index = new_index
     channel.basic_publish(
             exchange='',
@@ -61,9 +70,9 @@ def callback(ch, method, properties, body):
                 correlation_id=properties.correlation_id,
             ),
             body="")
-    print("sent final blank message")
+    debug("Sent final blank message")
 
 channel.basic_consume(queue=hostname, on_message_callback=callback, auto_ack=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+debug('Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 
